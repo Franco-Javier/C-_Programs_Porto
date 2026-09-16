f29b20f55cb1a79a884a5be8b610fd8712086476
Francisco Javier <franciscoherazo.work@gmail.com>
2026-09-16 09:53:33 -0500

    Add files via upload

---
+++ Parcial1_FranciscoHerazo/Problema1_switch.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics.Tracing;
+
+Console.WriteLine("Identificador Simple del Día de la Semana");
+
+Console.WriteLine("Ingrese un numero del 1 al 7:");
+int numeroid = Convert.ToInt32(Console.ReadLine());
+
+int diaSiguiente = numeroid + 1;
+
+string diaHoy = numeroid switch
+{
+    1 => "Lunes",
+    2 => "Martes",
+    3 => "Miercoles",
+    4 => "Jueves",
+    5 => "Viernes",
+    6 => "Sabado",
+    7 => "Domingo",
+    _ => "No hay equivalencia"
+};
+
+Console.WriteLine("El dia de hoy es: "+diaHoy );
+
+
---
+++ Parcial1_FranciscoHerazo/Problema_1_IF.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("Sistema de rendimiento Académico");
+
+Console.WriteLine("\nIngrese la calificacion del parcial 1");
+double Cal_1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("\nIngrese la calificacion del parcial 2");
+double Cal_2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("\nIngrese el porcentaje de asistencia (0-100): ");
+double asistencia = Convert.ToDouble(Console.ReadLine());
+
+
+double promedio_p = (Cal_1 + Cal_2) / 2;
+double bono = asistencia * 0.05;
+
+double calFinal = promedio_p + bono;
+
+string estado = "";
+if (asistencia < 60 || promedio_p < 70) {
+    estado = "El estudiante reprueba la asignatura (promedio y/o asistencia deficiente";
+}
+
+if ( asistencia > 60 && promedio_p > 70)
+{
+    estado = "Aprobado || no es candidato a excelencia académica";
+}
+
+if (Cal_1 >= 90 && Cal_2 >= 90)
+{
+    estado = "Aprobado || es candidato a excelencia académica";
+}
+
+Console.WriteLine("\nCalificación promedio de parciales: " + promedio_p);
+Console.WriteLine("\nCalificación final: " +calFinal);
+Console.WriteLine("\nEstado académico: " +estado);
